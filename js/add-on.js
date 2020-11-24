feec45f341f796b2572fa1cb9b6b314c9f930313
jgianoglio <jgianoglio@users.noreply.github.com>
2020-11-24 18:45:39 +0000

    deploy: 12a515c6b4aafd8ae83e7c4395f3b85facd2b6fc

--- js/add-on.js
+++ js/add-on.js
@@ -4,3 +4,10 @@
   is an official part of the theme (ex. Pull Requests) should be included in main.js
   and follow the formatting and style given.
 */
+/* Google Tag Manager */
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TFH36H');</script>
+/* End Google Tag Manager */
